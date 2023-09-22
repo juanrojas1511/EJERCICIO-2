e83ce8899ccead48853aceaaa15cdfcd078187db
Juan Paris <juanpajares6@gmail.com>
2023-09-21 20:35:20 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la base:");
+            int num1=int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el exponente:");
+            int num2=int.Parse(Console.ReadLine());
+            int resultado = 1;
+            for(int i = 0; i < num2; i++)
+            {
+                resultado *= num1;
+            }
+            Console.WriteLine("La potencia es:" + resultado);
+            Console.ReadKey();
+        }
+    }
+}
